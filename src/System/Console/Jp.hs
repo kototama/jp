205801159dfe39e96e03a895ee50993232bd58b4
kototama <kototamo@gmail.com>
2014-07-27 16:02:55 +0200

    add a St structure containing the state
    - it contains both the command-line options and the modules for the
      interpreter

--- src/System/Console/Jp.hs
+++ src/System/Console/Jp.hs
@@ -8,27 +8,31 @@ module System.Console.Jp
 import System.Environment (getArgs, getProgName)
 import System.Console.GetOpt (usageInfo)
 import System.Exit (exitFailure,exitSuccess)
-
 import qualified Data.ByteString.Lazy.Char8 as C
-
 import Text.PrettyPrint.ANSI.Leijen
 import System.Console.Jp.Pretty
-
 import Data.Aeson
-
 import System.Console.Jp.Interpreter
+import System.Console.Jp.Options as O
+import qualified System.Console.Jp.Modules as M
 
-import System.Console.Jp.Options
+data St = St { options :: Options,
+               modules :: [M.Module],
+               files :: [FilePath]
+             }
 
 getUsage :: IO String
 getUsage = do
     pn <- getProgName
-    return $ usageInfo ("Usage: " ++ pn ++ " [<options>] [<file>]") options
+    return $ usageInfo ("Usage: " ++ pn ++ " [<options>] [<file>]") O.options
 
 data JsonInput = JsonInput String Value
 
-processJSON :: Options -> JsonInput -> IO ()
-processJSON Options{ optColor = True, optExpr = Just expr, optMinimize = False} (JsonInput s _) = do
+processJSON :: St -> JsonInput -> IO ()
+processJSON St { options = Options{ optColor = True,
+                                    optExpr = Just expr,
+                                    optMinimize = False}}
+  (JsonInput s _) = do
   res <- runAesonLensInterpreter s expr
   case res of
      Right v -> putDoc (encodePretty v)
@@ -36,7 +40,8 @@ processJSON Options{ optColor = True, optExpr = Just expr, optMinimize = False} 
        putStr $ errMsg
        exitFailure
 
-processJSON Options{ optExpr = Just expr, optMinimize = True} (JsonInput s _) = do
+processJSON St { options = Options{ optExpr = Just expr, optMinimize = True}}
+  (JsonInput s _) = do
   res <- runAesonLensInterpreter s expr
   case res of
      Right v -> putJsonMinimized v
@@ -44,10 +49,12 @@ processJSON Options{ optExpr = Just expr, optMinimize = True} (JsonInput s _) = 
        putStr $ errMsg
        exitFailure
 
-processJSON Options{ optColor = True, optExpr = Nothing, optMinimize = False} (JsonInput _ v) = do
+processJSON St { options = Options{ optColor = True, optExpr = Nothing, optMinimize = False}}
+  (JsonInput _ v) = do
   putDoc $ encodePretty v
 
-processJSON Options{ optColor = True, optExpr = Nothing, optMinimize = True} (JsonInput _ v) = do
+processJSON St { options = Options{ optColor = True, optExpr = Nothing, optMinimize = True}}
+  (JsonInput _ v) = do
   putJsonMinimized v
 
 processJSON _ _ = do
@@ -58,31 +65,48 @@ putJsonMinimized :: Value -> IO ()
 putJsonMinimized v = putStr . (flip displayS "") $ doc
     where doc = renderCompact (encodePretty' compactConfig v)
 
-processInput :: Options -> String -> IO ()
-processInput opts input =
+processInput :: St -> String -> IO ()
+processInput st input =
   case eitherDecode (C.pack input) of
     Left errMsg -> do
       putStr $ errMsg
       exitFailure
     Right v ->
-      processJSON opts (JsonInput input v)
+      processJSON st (JsonInput input v)
 
+getModules :: Maybe FilePath -> IO (Either String [M.Module])
+getModules Nothing = return $ Right M.defaultModules
+getModules (Just path) = M.readModules path
 
-main :: IO ()
-main = do
-  args <- getArgs
+calculateState :: [String] -> IO St
+calculateState args = do
   o <- calculateOpts args
   case o of
     Left _ -> do
       getUsage >>= putStr
-      exitFailure
+      exitSuccess
     Right (Options {optPipe = False}, []) -> do
       getUsage >>= putStr
       exitSuccess
-    Right (opts@Options {optPipe = True}, _) -> do
+    Right (ops, fls) -> do
+      x <- getModules $ optModulesFile ops
+      case x of
+        Left msg -> do
+          putStr msg
+          exitSuccess
+        Right md ->
+          return St { options = ops,
+                      modules = md,
+                      files = fls }
+
+main :: IO ()
+main = do
+  args <- getArgs
+  st <- calculateState args
+  case st of
+    St { options = Options {optPipe = True}, files = []} -> do
       input <- getContents
-      processInput opts input
-    Right (opts, files) -> do
-      -- TODO: better repeat the process instead of concat
-      input <- fmap concat . mapM readFile $ files
-      processInput opts input
+      processInput st input
+    St { files = fls} -> do
+      input <- fmap concat . mapM readFile $ fls
+      processInput st input
--- src/System/Console/Jp/Modules.hs
+++ src/System/Console/Jp/Modules.hs
@@ -1,10 +1,19 @@
-module System.Console.Jp.Modules (readModules) where
-
+module System.Console.Jp.Modules (readModules,
+                                  defaultModules,
+                                  Module)
+       where
 import System.FilePath ()
 import Control.Monad (liftM)
 import Text.Read (readEither)
 
 type Module = (String, Maybe String)
+
+defaultModules :: [Module]
+defaultModules = [("Prelude", Nothing), 
+                   ("Data.Map", Just "M"), 
+                   ("Control.Lens", Nothing), 
+                   ("Data.Aeson.Lens", Nothing), 
+                   ("Data.Aeson", Nothing)]
 
 -- | Read and parse the modules file
 readModules :: FilePath -> IO (Either String [Module])
